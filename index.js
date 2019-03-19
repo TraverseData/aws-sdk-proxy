0c3eb2871b7d3f707cb1b6263eaa8643d57fa309
Sam Brown <sbrown2594@gmail.com>
2019-03-19 16:38:00 -0700

    1.0.4

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var AWS = require('aws-sdk');
 
 var proxy = process.env['HTTPS_PROXY'] || process.env['https_proxy'] || process.env['http_proxy'];
+var timeout = process.env['AWS_HTTP_TIMEOUT'] || 240000;
+var connectionTimeout = process.env['AWS_HTTP_CONNECTION_TIMEOUT'] || 240000;
+
 var isProduction = (typeof process.env['NODE_ENV'] !== 'undefined' && process.env['NODE_ENV'] === 'production');
 if (isProduction && proxy) {
   try {
     AWS.config.update({
       httpOptions: {
-        proxy: proxy
+        proxy: proxy,
+        timeout: timeout,
+        connectionTimeout: connectionTimeout
       },
     });
   } catch (e) {
