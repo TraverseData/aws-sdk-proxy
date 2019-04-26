50aa70e5f4b7f878fb716ae490cec421a7067374
justinherter <jherter80@gmail.com>
2019-04-26 12:59:25 -0700

    1.0.6

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var AWS = require('aws-sdk');
 
 var proxy = process.env['HTTPS_PROXY'] || process.env['https_proxy'] || process.env['http_proxy'];
-var timeout = process.env['AWS_HTTP_TIMEOUT'] || 240000;
-var connectionTimeout = process.env['AWS_HTTP_CONNECTION_TIMEOUT'] || 240000;
+var timeout = parseInt(process.env['AWS_HTTP_TIMEOUT']) || 240000;
+var connectionTimeout = parseInt(process.env['AWS_HTTP_CONNECTION_TIMEOUT']) || 240000;
 
 var isProduction = (typeof process.env['NODE_ENV'] !== 'undefined' && process.env['NODE_ENV'] === 'production');
 if (isProduction && proxy) {
